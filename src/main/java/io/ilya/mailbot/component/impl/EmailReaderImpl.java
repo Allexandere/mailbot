ce1c7933d14f4543eb272249cba7fa7457a03643
Allexandere <39259989+Allexandere@users.noreply.github.com>
2022-09-15 18:44:53 +0300

    changed some properties

--- src/main/java/io/ilya/mailbot/component/impl/EmailReaderImpl.java
+++ src/main/java/io/ilya/mailbot/component/impl/EmailReaderImpl.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 @Slf4j
 public class EmailReaderImpl implements EmailReader {
 
-    @Value("${LOGIN}")
+    @Value("${email.login}")
     private String LOGIN;
     @Value("${email.password}")
     private String PASSWORD;
